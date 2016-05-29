4c121b209f40b98e344571d3ff4d70d343e3397b
Aftermoth <Aftermoth@users.noreply.github.com>
2016-05-29 20:16:26 +1200

    Major rewrite
    - Making invoke safer, and incidentally more useful.
    - Stronger insurance against multiple timers on the same object.
    - Sync control for better integration with processes that may exploit it.
    - Optional actions provided by invoke removed from main lift cycle to reduce footprint.
    - Switched to os.time().
    - Removed surplus vel/acc calls.
    - Simpler and better escape calculation.
    - Chunk/session reloading has been more thoroughly tested with no flaws apparent.

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
-	
 --[[
 
 Copyright (C) 2016 Aftermoth, Zolan Davis
@@ -12,10 +11,17 @@ http://www.gnu.org/licenses/lgpl-2.1.html
 
 
 --]]
+--------------------------------------------------- Global
 
-droplift = {}
+droplift = {
+	invoke,
+	-- function (dropobj, sync)
+	-- sync in [ false | 0 | seconds ]. See details.txt
+}
 
-local function in_walkable(p)
+--------------------------------------------------- Local
+
+local function obstructed(p)
 	local n = minetest.get_node_or_nil(p)
 	return n and minetest.registered_nodes[n.name].walkable
 end
@@ -23,124 +29,107 @@ end
 
 -- * Local escape *
 
--- get nearest player in range (taxicab)
-local function near_player(dpos)
-	local near = 8.5
-	local pp, d, ppos
+local dist = function(p1,p2)
+	return (  (p1.y - p2.y)^2
+			+ (p1.x - p2.x)^2
+			+ (p1.z - p2.z)^2 )^0.5
+end
+
+local function escape(ent,pos)
+	local q, p, ep, d, dd = pos
 	for _,player in ipairs(minetest.get_connected_players()) do
-		pp = player:getpos()
-		pp.y = pp.y + 1
-		d = math.abs(pp.x-dpos.x) + math.abs(pp.y-dpos.y) + math.abs(pp.z-dpos.z)
-		if d < near then
-			near = d
-			ppos = pp
-		end
+		p = player:getpos(); p.y = p.y + 1
+		d = dist(p,pos)
+		if not dd or (d < dd) then dd, q = d, {x=p.x,y=p.y,z=p.z} end
 	end
-	if near < 8.5 then return ppos else return false end
-end
-
-
-local function usign(r)
-	if r < 0 then return -1 else return 1 end
-end
-
-
-local function quick_escape(ent,pos)
-
-	local bias = {x = 1, y = 1, z = 1}
-	local o = {a="x", b="y", c="z"}
-
-	local pref = near_player(pos)
-	if pref then
-		bias = {x = usign(pref.x - pos.x), y = usign(pref.y - pos.y), z = usign(pref.z - pos.z)}
-		local mag={x=math.abs(pref.x - pos.x), y=math.abs(pref.y - pos.y), z=math.abs(pref.z - pos.z)}
-		if mag.z > mag.y then
-			if mag.y > mag.x then
-				o={a="z",b="y",c="x"}
-			elseif mag.z > mag.x then
-				o={a="z",b="x",c="y"}
-			else
-				o={a="x",b="z",c="y"}
-			end
-		else
-			if mag.z > mag.x then
-				o={a="y",b="z",c="x"}
-			elseif mag.y > mag.x then
-				o={a="y",b="x",c="z"}
-			end
-		end
-	end
-
-	local p
-	for a = pos[o.a] + bias[o.a], pos[o.a] - bias[o.a], -bias[o.a] do
-		for b = pos[o.b] + bias[o.b], pos[o.b] - bias[o.b], -bias[o.b] do
-			for c = pos[o.c] + bias[o.c], pos[o.c] - bias[o.c], -bias[o.c] do
-				p = {[o.a]=a, [o.b]=b, [o.c]=c}
-				if not in_walkable(p) then
-					ent.object:setpos(p)
-					return p
+	for x = pos.x - 1, pos.x + 1 do
+		p.x = x; for y = pos.y - 1, pos.y + 1 do
+			p.y = y; for z = pos.z - 1, pos.z + 1 do
+				p.z = z
+				if not obstructed(p) then
+					d = dist(q,p)
+					if not ep or (d < dd) then dd, ep = d, {x=p.x,y=p.y,z=p.z} end
 				end
 			end
 		end
 	end
-
-	return false
+	if ep then ent.object:setpos(ep) end
+	return ep
 end
 
 
 -- * Entombment physics *
 
+-- ---------------- LIFT
 
-local function disentomb(obj, reset)
+local function lift(obj)
 	local p = obj:getpos()
 	if p then
-
 		local ent = obj:get_luaentity()
-		local w = in_walkable(p)
-		local brace = math.floor(p.y) + 0.800001
-
-		if ent.is_entombed then
-			local p2 = p
-			if w then
-				p2 = {x = p.x, y = brace + 1, z = p.z}
-				obj:setpos(p2)
+		if ent.is_entombed and obstructed(p) then
+-- Time
+			local t = 1
+			local s1 = ent.sync1
+			if s1 then
+				local sd = ent.sync0+s1-os.time()
+				if sd > 0 then t = sd end
+				ent.sync0, ent.sync1 = nil, nil
 			end
-			ent.is_entombed = in_walkable(p2)
-		elseif w and not (reset and quick_escape(ent,p)) then
-			obj:setpos({x = p.x, y = brace, z = p.z})
-			ent.is_entombed = true
+-- Space
+			p = {x = p.x, y = math.floor(p.y - 0.5) + 1.800001, z = p.z}
+			obj:setpos(p)
+			if s1 or obstructed(p) then
+				minetest.after(t, lift, obj)
+				return
+			end
 		end
-
-		if ent.is_entombed then
-			obj:setvelocity({x = 0, y = 0, z = 0})
-			obj:setacceleration({x = 0, y = 0, z = 0})
-			minetest.after(1.0, disentomb, obj, false)
-		end
-
+-- Void.
+		ent.is_entombed, ent.sync0, ent.sync1 = nil, nil, nil
 	end
 end
 
-function droplift.invoke(obj, entomb)
-	disentomb(obj, not entomb)
+-- ---------------- ASYNC
+
+local k = 0
+local function newhash()
+	k = (k==32767 and 1) or k+1
+	return k
 end
 
+local function async(obj, usync)
+	local p = obj:getpos()
+	if p then
+		local ent = obj:get_luaentity()
+		local hash = newhash()
+		ent.hash = ent.hash or hash
+		if obstructed(p) then
+-- Time.
+			if not usync then
+				if escape(ent, p) and  hash == ent.hash then
+					ent.hash = nil
+				end
+			elseif usync > 0 then
+				ent.sync0 = os.time()
+				ent.sync1 = usync
+			end
+-- Space.
+			if hash == ent.hash then
+				obj:setpos({x = p.x, y = math.floor(p.y - 0.5) + 0.800001, z = p.z})
+				if not ent.is_entombed then
+					ent.is_entombed = true
+					minetest.after(1, lift, obj)
+				end
+			end
+		end
+		if hash == ent.hash then ent.hash = nil end
+	end
+end
 
+droplift.invoke = function(obj, sync)
+	async(obj, (sync and math.max(0,sync)))
+end
 
 -- * Events *
-
-
--- Poll until defaults are ready before continuing.
-local function wait_itemstring(ent, c)
-	if ent.itemstring == "" then
-		if c < 10 then
-			minetest.after(0.1, wait_itemstring, ent, c + 1)
-		end
-		return
-	end
-
-	disentomb(ent.object, false)
-end
-
 
 local function append_to_core_defns()
 	local dropentity=minetest.registered_entities["__builtin:item"]
@@ -152,11 +141,11 @@ local function append_to_core_defns()
 		if staticdata ~= "" then 
 			if minetest.deserialize(staticdata).is_entombed then
 				ent.is_entombed = true
-				minetest.after(0.1, wait_itemstring, ent, 1)
-			else
-				ent.object:setvelocity({x = 0, y = 0, z = 0})
+				ent.object:setacceleration({x = 0, y = 0, z = 0})  -- Prevents 0.18m reload slippage. Not critical.
+				minetest.after(0.1, lift, ent.object)
 			end
 		end
+		ent.object:setvelocity({x = 0, y = 0, z = 0})  -- Prevents resting-buried drops burrowing. 
 	end
 
 	-- Preserve state across reloads
@@ -182,13 +171,12 @@ local function append_to_core_defns()
 		for _,obj in ipairs(a) do
 			local ent = obj:get_luaentity()
 			if ent and ent.name == "__builtin:item" then
-				disentomb(obj, true)
+				async(obj)
 			end
 		end
 	end
 
 end
-
 
 
 append_to_core_defns()
